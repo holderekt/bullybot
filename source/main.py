02bc5f8eeccd04b6347c6bf35efac6919afb3ca1
Holderekt <33879857+holderekt@users.noreply.github.com>
2020-10-01 23:23:18 +0200

    Neighbor functions and usable main

--- source/main.py
+++ source/main.py
@@ -1,73 +1,20 @@
-import source.lyricparser
-import source.markov
+import lyricparser
+import chainmarkov
 import random as rand
 
-
-
-
-        
-
-
-
-
 bars, dirty = lyricparser.parse('./data/message.txt')
-chain = Chain(bars)
+chain = chainmarkov.Chain(bars)
 
-while True:
-    print(chain.get_next("le"))
-    a = input()
-    
-
-
-
-
-'''
 while True:
     var = rand.choice(list(chain.chain.keys()))
     stop = True
     while stop:
         print(var, end=' ')
         if(not chain.is_last(var)):
-            elements = [element for element in chain.get_word_counts(var)]
-            coumulative_percentage = [0 for _ in range(len(elements))]
-            coumulative_percentage[0] = chain.get_frequency(var, elements[0])
-         
-            for index in range(1, len(elements)):
-                coumulative_percentage[index] = coumulative_percentage[index - 1] + chain.get_frequency(var, elements[index])
-
-            x = rand.random()
-            chosen_var = 0
-
-            for index in range(len(elements)):
-                if x <= coumulative_percentage[index]:
-                    chosen_var = elements[index]
-
-            var = chosen_var
+            var = chain.get_next(var)
         else:
-            if((len(chain.get_word_counts(var)) > 0) and (rand.random() >= 85)):
-      
-                    elements = [element for element in chain.get_word_counts(var)]
-                    coumulative_percentage = [0 for _ in range(len(elements))]
-                    coumulative_percentage[0] = chain.get_frequency(var, elements[0])
-                
-                    for index in range(1, len(elements)):
-                        coumulative_percentage[index] = coumulative_percentage[index - 1] + chain.get_frequency(var, elements[index])
-
-                    x = rand.random()
-                    chosen_var = 0
-
-                    for index in range(len(elements)):
-                        if x <= coumulative_percentage[index]:
-                            chosen_var = elements[index]
-
-                    var = chosen_var        
+            if((chain.neighbor_size(var) > 0) and (rand.random() >= 85)):
+                var = chain.get_next(var)  
             else:
                 a = input()
                 stop = False
-'''
-
-
-            
-            
-
-
