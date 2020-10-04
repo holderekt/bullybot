9436904b28ad60b8099e2306e577d03a10fdff69
Ivan Diliso <diliso.ivan@gmail.com>
2020-10-04 15:57:05 +0200

    Better lyrics file

--- source/main.py
+++ source/main.py
@@ -3,16 +3,7 @@ import chainmarkov
 import spitter
 import random as rand
 
-bars, dirty = lyricparser.parse('./data/sample.txt')
-
-for bar in bars:
-    print(bar)
-
-print(" -- ")
-for bar in dirty:
-    print(bar)
-
-'''
+bars, dirty = lyricparser.parse('./data/lyrics.txt')
 chain = chainmarkov.BarsChain(bars)
 dirtychain = chainmarkov.DirtyChain(dirty)
 spitter = spitter.Spitter(chain, dirtychain)
@@ -20,4 +11,4 @@ spitter = spitter.Spitter(chain, dirtychain)
 while(True):
     print(spitter.spit(), end="")
     a = input()
-'''
+
